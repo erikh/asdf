4a1dc8f8b2f693a67b8da2e738a96471d5e02728
Erik Hollensbe <git@hollensbe.org>
2026-05-31 17:44:14 -0700

    eh
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/lib.rs
+++ src/lib.rs
@@ -3,34 +3,69 @@ pub type Port = usize;
 pub type Position = usize;
 
 #[derive(Debug, Clone)]
-pub struct Network<const SIZE: usize>([usize; SIZE]);
+pub struct Network<const SIZE: usize, const ADDR_SIZE: usize>([Node; SIZE]);
 
-impl<const SIZE: usize> Default for Network<SIZE> {
+impl<const SIZE: usize, const ADDR_SIZE: usize> Default for Network<SIZE, ADDR_SIZE> {
     fn default() -> Self {
-        Network([0_usize; SIZE])
+        let mut v = Vec::new();
+        for i in 0..SIZE {
+            v.push(Node::new_pos(i))
+        }
+
+        Self(v.as_array().unwrap().clone())
     }
 }
 
-pub struct Address<const SIZE: usize> {
-    network: Network<SIZE>,
+#[derive(Debug, Clone, Default)]
+pub struct Address {
     position: Position,
     port: Port,
 }
 
-pub struct Node<const SIZE: usize> {
-    address: Address<SIZE>,
+#[derive(Debug, Clone, Default)]
+pub struct Node {
+    address: Address,
     name: String,
     connected: bool,
 }
 
+impl Node {
+    pub fn new_pos(pos: usize) -> Self {
+        Self {
+            address: Address {
+                position: pos,
+                port: 0,
+            },
+            name: "default".to_string(),
+            connected: false,
+        }
+    }
+}
+
 pub type PropagateResult = anyhow::Result<(bool, bool)>;
 
-impl<const SIZE: usize> Network<SIZE> {
+impl<const ORIG: usize, const NEW: usize, const ADDR_SIZE: usize> From<[Node; ORIG]>
+    for Network<NEW, ADDR_SIZE>
+{
+    fn from(value: [Node; ORIG]) -> Self {
+        let mut this = Self::default();
+        for (x, item) in value.iter().enumerate() {
+            this.0[x] = item.clone()
+        }
+
+        this
+    }
+}
+
+impl<const SIZE: usize, const ADDR_SIZE: usize> Network<SIZE, ADDR_SIZE> {
     pub fn new() -> Self {
         Self::default()
     }
 
-    pub fn propagate() -> PropagateResult {
-        Ok((true, true))
+    // returns a cursor
+    pub fn find_peers<const BATCH_SIZE: usize>(
+        &self,
+    ) -> anyhow::Result<(usize, Network<BATCH_SIZE, ADDR_SIZE>)> {
+        Ok((BATCH_SIZE, self.0.clone().into()))
     }
 }
